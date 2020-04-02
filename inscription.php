3f7ab86dd8c1f3ae268a2d271f5c40eb01f31ed8
Yann Radigon <Votre@email.com>
2020-04-02 21:57:57 +0200

    Formulaire inscription

--- inscription.php
+++ inscription.php
@@ -1,23 +1,153 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Page de traitement</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <p>Dans le formulaire précédent, vous avez fourni les
-        informations suivantes :</p>
+<?php
+
+    $login = htmlspecialchars($_POST['login']);
+    $mail = htmlspecialchars($_POST['mail']);
+    $mail2 = htmlspecialchars($_POST['mail2']);
+    $mdp = $_POST['mdp'];
+    $mdp2 = $_POST['mdp2'];
+    $nom = htmlspecialchars($_POST['nom']);
+    $numero = $_POST['numero'];
+    $rue = htmlspecialchars($_POST['rue']);
+    $cp = $_POST['cp'];
+    $ville = htmlspecialchars($_POST['ville']);
+    $cb = $_POST['cb'];
+
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=projet', 'root', 'root');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    if(isset($_POST['forminscription']))
+        {
         
-        <?php
-            echo 'Login : '.$_POST["nom"].'<br>';
-            echo 'Mot de passe : ' .$_POST["mdp"].'<br>';
-            echo 'mail : ' .$_POST["mail"].'<br>';
-            echo 'nom : ' .$_POST["nom"].'<br>';
-            echo 'numero : ' .$_POST["numero"].'<br>';
-            echo 'rue : ' .$_POST["rue"].'<br>';
-            echo 'cp : ' .$_POST["cp"].'<br>';
-            echo 'ville : ' .$_POST["ville"].'<br>';
-            echo 'cb : ' .$_POST["cb"].'<br>';
-        ?>
-    </body>
+    
+    
+        if(!empty($_POST['login']) AND !empty($_POST['mdp']) AND !empty($_POST['mdp2']) AND !empty($_POST['mail']) AND !empty($_POST['mail2']) AND !empty($_POST['nom']) AND !empty($_POST['numero']) AND !empty($_POST['rue']) AND !empty($_POST['cp']) AND !empty($_POST['ville']) AND !empty($_POST['cb']) )
+        {   
+    
+            $loginlenght = strlen($login);
+            if($loginlenght <= 20) 
+            {
+                if($mail == $mail2)
+                {
+                    if(filter_var($mail, FILTER_VALIDATE_EMAIL))
+                    {
+                        if($mdp == $mdp2)
+                        {
+                        $sth = $bdd->prepare("INSERT INTO visiteur(login, mdp, mail, nom, numero, rue, cp, ville, cb) VALUES (:login, :mdp, :mail, :nom, :numero, :rue, :cp, :ville, :cb)");  
+                        $sth->bindParam(':login',$login);
+                        $sth->bindParam(':mdp',$mdp);
+                        $sth->bindParam(':mail',$mail);
+                        $sth->bindParam(':nom',$nom);
+                        $sth->bindParam(':numero',$numero);
+                        $sth->bindParam(':rue',$rue);
+                        $sth->bindParam(':cp',$cp);
+                        $sth->bindParam(':ville',$ville);
+                        $sth->bindParam(':cb',$cb);
+                        $sth->execute();
+                        $erreur = "Votre compte a bien été créé";
+                        }
+                        else 
+                        {
+                            $erreur = "Vos mdp ne correpsondent pas";
+                        }
+                    }
+                    else 
+                    {
+                        $erreur = "Votre adresse mail n'est pas valide";
+                    }
+                }
+                else 
+                {
+                    $erreur = "Vos mail ne correpsondent pas";
+                }
+            } 
+            else 
+            {
+                $erreur = "Login doit être inférieur à 20 caratères";
+            }
+        }
+        else 
+        {
+          $erreur = "Tous les champs doivent être complétés";
+        }
+    }
+    
+} catch(PPDOException $Exception)
+{
+    echo 'Impossible de traiter les données. Erreur : '.$Exception->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Formulaire HTML</h1>
+
+    <form action="" method="POST">
+        <div>
+            <label for="pseudo">Login : </label>
+            <input type="text" id="login" name="login" value="<?php if(isset($login)){ echo $login; } ?>">
+        </div>
+        <div>
+            <label for="mdp">Mot de passe : </label>
+            <input type="password" id="mdp" name="mdp">
+        </div>
+        <div>
+            <label for="mdp2">Confirmation du mot de passe : </label>
+            <input type="password" id="mdp2" name="mdp2">
+        </div>
+        <div>
+            <label for="mail">Email : </label>
+            <input type="email" id="mail" name="mail" value="<?php if(isset($mail)){ echo $mail; } ?>">
+        </div>
+        <div>
+            <label for="mail2">Confirmation de mon email : </label>
+            <input type="email" id="mail2" name="mail2" value="<?php if(isset($mail2)){ echo $mail2; } ?>">
+        </div>
+        <div>
+            <label for="nom">Nom : </label>
+            <input type="text" id="nom" name="nom" value="<?php if(isset($nom)){ echo $nom; } ?>">
+        </div>
+        <div>
+            <label for="numero">Numéro de résidence : </label>
+            <input type="number" id="numero" name="numero" value="<?php if(isset($numero)){ echo $numero; } ?>">
+        </div>
+        <div>
+            <label for="rue">Rue : </label>
+            <input type="text" id="rue" name="rue" value="<?php if(isset($rue)){ echo $rue; } ?>">
+        </div>
+        <div>
+            <label for="cp">Code postal : </label>
+            <input type="number" id="cp" name="cp" value="<?php if(isset($cp)){ echo $cp; } ?>">
+        </div>
+        <div>
+            <label for="ville">Ville : </label>
+            <input type="text" id="ville" name="ville" value="<?php if(isset($ville)){ echo $ville; } ?>">
+        </div>
+        <div>
+            <label for="cb">Carte bancaire : </label>
+            <input type="number" id="cb" name="cb" value="<?php if(isset($cb)){ echo $cb; } ?>">
+        </div>
+        <div id="submit">
+            <input type="submit" value="Je m'inscris" name="forminscription">
+        </div>
+    </form>
+    <p><?php
+        if(isset($erreur))
+        {
+            echo $erreur;
+        }
+        ?></p>
+</body>
+
 </html>
