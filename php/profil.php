ba3576d929c6e8dbb35a7f927f5dad98a9265ccb
yannrdg <yann.radigon@gmail.com>
2020-06-13 16:42:24 +0200

    Création d'une page profil dans le site
    Chaque utilisateur peut consulter les informations de son profil mais également
    les modifier.
    
    Pour cela j'ai créé un boutton qui au click (dans le fichier profil.js), grâce
    à la fonction addEventListener() afficher une section de modification du profil
    au dépend de la section de présentation du profil dans la même page.
    
    La section modifierInfos affiche dans des input les informations du compte. Le
    contenu de chaque input (autre que login adresse mail et mdp car cela pose un
    problème pour vérifier que le login ou le mdp ne sont pas déjà utilisés) peut
    être modifié.
    
    Au click sur le input type="submit" modifier, grâce à une requête SQL
    (UPDATE...SET..) le contenu de chaque input remplace le contenu dans chaque
    colonne de la bdd et modifie par conséquent les informations du compte.

--- php/profil.php
+++ php/profil.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+if($_SESSION['idp'])
+{
 include 'config.php';
 $bdd = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
 $profil = $bdd->prepare("SELECT * FROM visiteur WHERE idp = :idp");
@@ -9,44 +11,75 @@ $infoProfil = $profil->fetchAll();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../style/global.css">
+    <link rel="stylesheet" href="../style/profil.css">
     <script src="../script/profil.js" async></script>
     <title>Profil</title>
 </head>
+
 <body>
     <?php
     include '../includes/header.php';
     ?>
     <main>
-    <button id="modifierInfos">Modifier le profil</button>
-    <button id="annuler">Annuler</button>
-    <?php
+        <h2>Mon profil</h2>
+        <?php
     foreach($infoProfil as $infos):  
-    ?>  
+    ?>
         <section id=infos>
-            <p><?=$infos['login']?></p>
-            <p><?=$infos['mail']?></p>
-            <p><?=$infos['nom']?></p>
-            <p><?=$infos['numero']?></p>
-            <p><?=$infos['rue']?></p>
-            <p><?=$infos['cp']?></p>
-            <p><?=$infos['ville']?></p>
-            <p><?=$infos['cb']?></p>
+            <p><i>Si vous avez modifié vos informations, merci d'actualiser la page pour voir ces modifications !</i></p>
+            <h5>Mes informations personnelles</h5>
+            <p><i>Login : </i><?=$infos['login']?></p>
+            <p><i>Mon adresse mail : </i><?=$infos['mail']?></p>
+            <p><i>Nom de famille : </i><?=$infos['nom']?></p>
+            <p><i>Mon numéro de téléphone : </i>0<?=$infos['numero']?></p>
+            <h5>Mon adresse de livraison</h5>
+            <p><i>Rue : </i><?=$infos['rue']?></p>
+            <p><i>Code postal : </i><?=$infos['cp']?></p>
+            <p>Ville : <?=$infos['ville']?></p>
+            <h5>Mes coordonnées bancaire</h5>
+            <p><i>Mon numéro de carte bancaire : </i><?=$infos['cb']?></p>
         </section>
         <section id="modif">
             <form action="" method="post">
-            <input type="text" value="<?=$infos['nom']?>" name="newNom" maxlength="20">
-            <input type="number" value="<?=$infos['numero']?>" name="newNumero" size="15" maxlength="11">
-            <input type="text" value="<?=$infos['rue']?>" name="newRue" maxlength="100">
-            <input type="number" value="<?=$infos['cp']?>" name="newCp" maxlength="5">
-            <input type="text" value="<?=$infos['ville']?>" name="newVille" maxlength="50">
-            <input type="number" value="<?=$infos['cb']?>" name="newCb" maxlength="16">
-            <input type="submit" name="modifier" value="Modifier">
+                <h5>Mes informations personnelles</h5>
+                <div>
+                    <label for="nom">Nom de famille <i>(En majuscule)</i> : </label>
+                    <input type="text" value="<?=$infos['nom']?>" name="newNom" maxlength="20" id="nom">
+                </div>
+                <div>
+                    <label for="numero">Mon numéro de téléphone : </label>
+                    <input type="number" value="0<?=$infos['numero']?>" name="newNumero" size="15" maxlength="11"
+                        id="numero">
+                </div>
+                <h5>Mon adresse de livraison</h5>
+                <div>
+                    <label for="rue">Rue : </label>
+                    <input type="text" value="<?=$infos['rue']?>" name="newRue" maxlength="100" id="rue">
+                </div>
+                <div>
+                    <label for="cp">Code postal : </label>
+                    <input type="number" value="<?=$infos['cp']?>" name="newCp" maxlength="5" id="cp">
+                </div>
+                <div>
+                    <label for="ville">Ville : </label>
+                    <input type="text" value="<?=$infos['ville']?>" name="newVille" maxlength="50" id="ville">
+                </div>
+                <h5>Mes coordonées bancaire</h5>
+                <div>
+                    <label for="cb">Mon numéro de carte bancaire : </label>
+                    <input type="number" value="<?=$infos['cb']?>" name="newCb" maxlength="16" id="cb">
+                </div>
+                <div>
+                    <input type="submit" name="modifier" value="Modifier" id="modifier">
+                    <button id="annuler">Annuler</button>
+                </div>
             </form>
-    <?php       
+            <?php       
     endforeach;
     $newLogin = htmlspecialchars($_POST['newLogin']);
     $newMail = htmlspecialchars($_POST['newMail']);
@@ -62,13 +95,14 @@ $infoProfil = $profil->fetchAll();
         {
             if(is_numeric($newNumero) == true )
             {
-                $sth = $bdd->prepare("UPDATE visiteur SET nom = :nom, numero = :numero, rue = :rue, cp = :cp, ville = :ville, cb = :cb");  
+                $sth = $bdd->prepare("UPDATE visiteur SET nom = :nom, numero = :numero, rue = :rue, cp = :cp, ville = :ville, cb = :cb WHERE idp = :idp");  
                 $sth->bindParam(':nom',$newNom);
                 $sth->bindParam(':numero',$newNumero);
                 $sth->bindParam(':rue',$newRue);
                 $sth->bindParam(':cp',$newCp);
                 $sth->bindParam(':ville',$newVille);
                 $sth->bindParam(':cb',$newCb);
+                $sth->bindParam(':idp',$_SESSION['idp']);
                 $sth->execute();
                 header("refresh:0;url=profil.php");
             }
@@ -83,7 +117,17 @@ $infoProfil = $profil->fetchAll();
         }
     }
     ?>
-            </section>
+        </section>
+        <button id="modifierInfos">Modifier le profil</button>
     </main>
+    <?php
+        include '../includes/footer.php';
+    ?>
 </body>
 </html>
+<?php
+}
+else
+{
+    header('Location: connexion.php');
+}
